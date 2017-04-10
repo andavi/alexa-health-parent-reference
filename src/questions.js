8b371e112c2b926bfddf52a42aaa601d518d3d8b
Andrew Davis <andrew.bradley.davis@gmail.com>
2017-04-10 09:49:02 -0400

    modularized exploratory mode logic

---
+++ src/questions.js
@@ -0,0 +1,46 @@
+module.exports = [{
+        "Reindeer have very thick coats, how many hairs per square inch do they have?": [
+            "13,000",
+            "1,200",
+            "5,000",
+            "700",
+            "1,000",
+            "120,000"
+        ]
+    },
+    {
+        "The 1964 classic Rudolph The Red Nosed Reindeer was filmed in. ": [
+            "Japan",
+            "United States",
+            "Finland",
+            "Germany",
+            "Canada",
+            "Norway",
+            "France"
+        ]
+    },
+    {
+        "Santas reindeer are cared for by one of the Christmas elves, what is his name?": [
+            "Wunorse Openslae",
+            "Alabaster Snowball",
+            "Bushy Evergreen",
+            "Pepper Minstix"
+        ]
+    },
+    {
+        "If all of Santas reindeer had antlers while pulling his Christmas sleigh, they would all be": [
+            "Girls",
+            "Boys",
+            "Girls and boys",
+            "No way to tell"
+        ]
+    },
+    {
+        "What do Reindeer eat?": [
+            "Lichen",
+            "Grasses",
+            "Leaves",
+            "Berries"
+        ]
+    }
+]
